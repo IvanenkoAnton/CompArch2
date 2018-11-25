a6ba397ce4b288d5924621603c0913a885d54a25
Olha Tovstenko <kerono4ka@gmail.com>
2018-11-26 00:38:35 +0200

    Add profiler

--- main.go
+++ main.go
@@ -3,43 +3,66 @@ package main
 import (
 	"./levenshtein"
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"runtime/pprof"
 	"sort"
-	"time"
 )
 
 func main() {
 
-	var args = os.Args[1:]
+	if len(os.Args) > 0 {
+		var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+		var memprofile = flag.String("memprofile", "", "write mem profile to file")
+		flag.Parse()
+		if *cpuprofile != "" {
+			f, err := os.Create(*cpuprofile)
+			if err != nil {
+				log.Fatal(err)
+			}
+			pprof.StartCPUProfile(f)
 
-	if args[0] == "test-drive" {
+		}
+
 		testdrive()
+
+		defer pprof.StopCPUProfile()
+
+		if *memprofile != "" {
+			f, err := os.Create(*memprofile)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			pprof.WriteHeapProfile(f)
+			f.Close()
+		}
+
+		fmt.Println("Done!")
 	} else {
+
 		simple()
 	}
 }
 
 func simple() {
-	var values = []string{"Asheville", "Arizona"}
+	var words = []string{"Asheville", "Arizona"}
 	var key = "Aclahoma"
-	sort.Sort(levenshtein.ByLeveshtein{Value: values, Key: key})
-	fmt.Println("Result: ", values)
+	fmt.Println("Words: ", words)
+	fmt.Println("Key: ", key)
+
+	sort.Sort(levenshtein.ByLeveshtein{Value: words, Key: key})
+
+	fmt.Println("Result: ", words)
 }
 
 func testdrive() {
 	var words = getwords()
 	var key = "Aclahoma"
-	fmt.Println("Words: ", words)
-	fmt.Println("Key: ", key)
 
-	var t1 = time.Now()
 	sort.Sort(levenshtein.ByLeveshtein{Value: words, Key: key})
-	var t2 = time.Now()
-
-	fmt.Println("Result: ", words)
-	fmt.Println("Time (s): ", float64(t2.UnixNano()-t1.UnixNano())/100000000)
 }
 
 func getwords() []string {
